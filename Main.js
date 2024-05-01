f29f1c2a91e5452e18d464e5d5962cdff02a2566
KashishKumar9896 <Kashishkumar4321@gmail.com>
2024-05-01 10:00:39 +0530

    Colour modification

--- Main.js
+++ Main.js
@@ -8,6 +8,11 @@ let toggle_circle = document.querySelector('.toggle_circle');
 let night_mode = document.querySelector('.night_mode');
 let body = document.querySelector('body');
 
+let text_input = document.querySelector('.text');
+let change_color = document.querySelector('.change_light_color');
+let colors_input = document.querySelector('.colors_input');
+let keyboard_lights = document.querySelector('.keyboard_lights');
+let keyboard_wrapp = document.querySelector('.keyboard_wrapp');
 
 
 for(let i = 0; i < keys.length; i++) {
