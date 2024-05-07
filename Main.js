5d12ceadb62a9be8f931c8cd7277ae427d5fbe5e
KashishKumar9896 <Kashishkumar4321@gmail.com>
2024-05-07 15:58:06 +0530

    Added Keyboard light

--- Main.js
+++ Main.js
@@ -9,8 +9,8 @@ let body = document.querySelector('body');
 let text_input = document.querySelector('.text');
 let change_color = document.querySelector('.change_light_color');
 let colors_input = document.querySelector('.colors_input');
-// let keyboard_lights = document.querySelector('.keyboard_lights');
-// let keyboard_wrapp = document.querySelector('.keyboard_wrapp');
+let keyboard_lights = document.querySelector('.keyboard_lights');
+ let keyboard_wrapp = document.querySelector('.keyboard_wrapp');
 
 for(let i = 0; i < keys.length; i++) {
     keys[i].setAttribute('keyname', keys[i].innerText);
